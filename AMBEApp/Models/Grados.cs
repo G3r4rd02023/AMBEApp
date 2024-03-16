8f71158ce9063e0c739564a681c4b2a7790f42d4
V1ck105 <vickykarinacortes998@gmail.com>
2024-03-16 00:05:00 -0600

    ......

---
+++ AMBEApp/Models/Grados.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AMBEApp.Models
+{
+  public class Grados
+    {
+        [JsonPropertyName("idGrado")]
+        public int IdGrado { get; set; }
+
+        [JsonPropertyName("idInstituto")]
+        public int IdInstituto { get; set; }
+
+        [JsonPropertyName("grado")]
+        public string Grado { get; set; }
+
+        [JsonPropertyName("estado")]
+        public string Estado { get; set; }
+
+        [JsonPropertyName("creadoPor")]
+        public string CreadoPor { get; set; }
+
+        [JsonPropertyName("fechaDeCreacion")]
+        public DateTime FechaDeCreacion { get; set; }
+
+        [JsonPropertyName("modificadoPor")]
+        public string ModificadoPor { get; set; }
+
+        [JsonPropertyName("fechaDeModificacion")]
+        public DateTime FechaDeModificacion { get; set; }
+
+    }
+}
---
+++ AMBEApp/ViewModels/GradosViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AMBEApp.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace AMBEApp.ViewModels
+{
+    public class GradosViewModel : INotifyPropertyChanged
+    {
+        private List<Grados> _grados;
+
+        public List<Grados> Grados
+        {
+            get => _grados;
+            set
+            {
+                _grados = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+         
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+ }
