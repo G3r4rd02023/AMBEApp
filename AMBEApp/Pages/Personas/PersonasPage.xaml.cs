0a65395135ba5d6ff6398345cb9ba2681049d2fb
Gerardo Lanza <glanza007@gmail.com>
2024-03-21 09:32:28 -0600

    filtrado automatico peersonas

--- AMBEApp/Pages/Personas/PersonasPage.xaml.cs
+++ AMBEApp/Pages/Personas/PersonasPage.xaml.cs
@@ -14,13 +14,53 @@ public partial class PersonasPage : ContentPage
         _viewModel = new PersonasViewModel();
         BindingContext = _viewModel;
         CargarPersonas();
+        pickerPersona.SelectedIndexChanged += (sender, e) => FiltrarPersonas();
+       
     }
 
     private async void CargarPersonas()
-    {              
-        ServicioTipoPersona servicioTipoPersona = new();
-        var tipoPersonas = await servicioTipoPersona.ObtenerLista();       
-        pickerPersona.ItemsSource = tipoPersonas.Select(p => p.TipoPersona).ToList();
+    {
+        try
+        {           
+            ServicioTipoPersona servicioTipoPersona = new();
+            var tipoPersonas = await servicioTipoPersona.ObtenerLista();
+            pickerPersona.ItemsSource = tipoPersonas.Select(p => p.TipoPersona).ToList();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Ha ocurrido un error al cargar los tipos de personas: {ex.Message}", "OK");
+        }
+    }
+
+    private async void FiltrarPersonas()
+    {
+        try
+        {
+            if (pickerPersona.SelectedItem != null)
+            {
+                ServicioTipoPersona servicioTipoPersona = new();
+                string nombreTipoPersona = pickerPersona.SelectedItem.ToString();
+                int idTipoPersona = await servicioTipoPersona.ObtenerIdTipoPersonaPorNombre(nombreTipoPersona);
+
+                if (idTipoPersona != -1)
+                {
+                    ServicioPersonas servicioPersonas = new();
+                    var personas = await servicioPersonas.ObtenerLista();
+                    var personasFiltradas = personas.Where(p => p.IdTipoPersona == idTipoPersona).ToList();
+
+                    _viewModel.Personas = new List<Persona>(personasFiltradas);
+                }
+                else
+                {
+                    // Manejar el caso donde no se encuentra el tipo de persona correspondiente al nombre seleccionado
+                    await DisplayAlert("Error", "El tipo de persona seleccionado no existe.", "OK");
+                }
+            }            
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Ha ocurrido un error al filtrar las personas: {ex.Message}", "OK");
+        }
     }
 
     private void OnGenerarPdfClicked(object sender, EventArgs e)
@@ -41,28 +81,5 @@ public partial class PersonasPage : ContentPage
 
     }
 
-    private async void FiltrarPersonas(object sender, EventArgs e)
-    {
-        try
-        {
-            if (!ServicioValidaciones.ValidarPicker(pickerPersona))
-            {
-                await DisplayAlert("Error", "Por favor, selecciona una opcion.", "OK");
-                return;
-            }
-
-            ServicioTipoPersona servicioTipoPersona = new();
-            string nombreTipoPersona = pickerPersona.SelectedItem.ToString();
-            int idTipoPersona = await servicioTipoPersona.ObtenerIdTipoPersonaPorNombre(pickerPersona.SelectedItem.ToString());
-            ServicioPersonas servicioPersonas = new();
-            var personas = await servicioPersonas.ObtenerLista();
-            var personasFiltradas = personas.Where(p => p.IdTipoPersona == idTipoPersona).ToList();
-
-            _viewModel.Personas = new List<Persona>(personasFiltradas);           
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Error", $"Ha ocurrido un error: {ex.Message}", "OK");
-        }
-    }
+    
 }
